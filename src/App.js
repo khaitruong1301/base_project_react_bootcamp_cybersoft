249a41d1bdab0bf02d36b1623b339d2b94818754
macbook <khaitruong1301@gmail.com>
2022-06-06 23:36:07 +0700

    dp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import HomePage from './pages/HomePage/HomePage';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import HomeTemplate from './templates/HomeTemplate';
+
+export const history = createBrowserHistory();
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <HomeTemplate exact path={'/login'} component={Login} />
+        <HomeTemplate exact path={'/register'} component={Register} />
+
+        <HomeTemplate exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//setup antd
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+//setup redux
+import {Provider} from 'react-redux'
+import { store } from './redux/configStore';
+//setupscss
+import './assets/scss/_styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
